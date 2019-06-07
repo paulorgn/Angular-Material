e3718ca65a04526b27675b553c5f3f1c971c3270
paulorgn <paulorgndev@gmail.com>
2019-06-07 18:45:24 -0300

    Forms

--- AngularCLIMaterial/src/app/app.module.ts
+++ AngularCLIMaterial/src/app/app.module.ts
@@ -3,7 +3,12 @@ import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatFormField} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import { DemoMaterialModule } from './material-module';
 
 @NgModule({
   declarations: [
@@ -12,8 +17,13 @@ import {MatButtonModule, MatCheckboxModule} from '@angular/material';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     MatButtonModule,
+    DemoMaterialModule,
     MatCheckboxModule
   ],
   providers: [],
